5f0f57d1949d63c3a89021005dab4c247b22eb2e
jay97 <jayminer97@gmail.com>
2024-05-29 15:12:40 -0400

    Update touring.js
    added spider gang II pics/link

--- js/content/touring.js
+++ js/content/touring.js
@@ -3,9 +3,9 @@ export const LEAD_TEXT =
 
 export const TOURS = [
   { label: "the temple of doom 2024 (coming soon)", value: "#nothing" },
-  { label: "lil darkie loves schnitzel 2024", value: "#nothing" },
+  { label: "lil darkie loves schnitzel 2024 (coming soon)", value: "#nothing" },
   { label: "the collapse of modern society 2023 (fall)", value: "#nothing" },
   { label: "dark army europe 2023 (spring)", value: "#nothing" },
   { label: "dark army 2023 (winter)", value: "#nothing" },
-  { label: "spider gang II 2022 (summer)", value: "#nothing" },
+  { label: "spider gang II 2022 (summer)", value: "https://flic.kr/s/aHBqjBswAr" },
 ];
