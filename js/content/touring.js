8605dd6b87d861c2450f944b3b89a69b4df78baf
jay97 <jayminer97@gmail.com>
2024-08-02 22:52:10 -0700

    Update touring.js
    added eu pics

--- js/content/touring.js
+++ js/content/touring.js
@@ -2,8 +2,8 @@ export const LEAD_TEXT =
   "live concert tour management / logistics / merchandising. here is a list of tours we have executed";
 
 export const TOURS = [
-  { label: "the temple of doom 2024 (coming soon)", value: "https://www.lildarkie.com/temple-of-doom-tour" },
-  { label: "lil darkie loves schnitzel 2024 (coming soon)", value: "https://www.lildarkie.com/tour-2024" },
+  { label: "the temple of doom 2024 (on sale now!)", value: "https://www.lildarkie.com/temple-of-doom-tour" },
+  { label: "lil darkie loves schnitzel 2024", value: "https://flic.kr/s/aHBqjBC6ez" },
   { label: "the collapse of modern society 2023 (fall)", value: "https://flic.kr/s/aHBqjBsxvH" },
   { label: "darkie europe 2023 (spring)", value: "https://flic.kr/s/aHBqjBsDh9" },
   { label: "dark army 2023 (winter)", value: "https://flic.kr/s/aHBqjBsA2D" },
